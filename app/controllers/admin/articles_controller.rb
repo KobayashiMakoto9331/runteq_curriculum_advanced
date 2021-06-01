b0a0e3f9b41f020357e1600f68b8ea53b86c9431
小林誠 <kobax0@gmail.com>
2021-06-01 23:26:06 +0900

    ADD:日本語訳 に公開待ちを追加

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -35,18 +35,17 @@ class Admin::ArticlesController < ApplicationController
     authorize(@article)
 
     if @article.update(article_params)
-      if @article.state == 'draft'#下書きならそのまま
+      if @article.state == 'draft' # 下書きならそのまま
         @article.draft!
         flash[:notice] = '記事を更新しました'
-      elsif Time.current >= @article.published_at #現在時刻が公開日より後なら、公開に変更
+      elsif Time.current >= @article.published_at # 現在時刻が公開日より後なら、公開に変更
         @article.published!
         flash[:notice] = '記事を公開しました'
-      elsif Time.current < @article.published_at #公開日が未来になっているのは、公開待ち
+      elsif Time.current < @article.published_at # 公開日が未来になっているのは、公開待ち
         @article.publish_wait!
         flash[:notice] = '記事を公開待ちにしました'
       end
 
-      
       redirect_to edit_admin_article_path(@article.uuid)
     else
       render :edit
